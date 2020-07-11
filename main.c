943249bb81de413740b134366f998508e3e26d75
skyl1n3-00 <45659644+skyl1n3-00@users.noreply.github.com>
2020-07-11 21:57:13 +0100

    Delete main.c

--- main.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include "map_si_functions.h"
-
-void main(){
-  map_string_int_ll *list = (map_string_int_ll*)malloc(sizeof(map_string_int_ll));
-  free(list);
-}
